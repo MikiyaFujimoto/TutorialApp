b19776ec2eed62bb2c8dbc59483638513c3c025f
Mikiya Fujimoto <1910370142t@kindai.ac.jp>
2021-07-08 22:59:10 +0900

    Initial Commit

---
+++ TutorialApp/TutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialAppApp.swift
+//  TutorialApp
+//
+//  Created by 藤本幹也 on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct TutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
